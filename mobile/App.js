7814ea8d30f6d64ab344726adf26fa96f59dcf6d
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2024-02-21 23:45:52 -0500

    update

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,215 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image, ScrollView, Dimensions,Animated, ImageBackground } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { HambergerMenu } from 'iconsax-react-native';
+import { BlurView } from 'expo-blur';
+import { ArrowLeft2, Scroll,Heart,Pause } from 'iconsax-react-native';
+import { ye } from './config/temp';
+import { useEffect, useRef, useState } from 'react';
+ 
 
 export default function App() {
+  const [play, setPlay] = useState(false);
+  const [count, setCount] = useState(1);
+  const [dataArray, setDataArray] = useState(ye["1"]); // Initial data array based on count
+  const totalItems = Object.keys(ye).length; // Total number of arrays in ye
+
+  const animatedValues = useRef(dataArray.map(() => new Animated.Value(0))).current;
+  const calculateHeight = (value, index) => {
+    const period = 2 * Math.PI / dataArray.length; // Adjust the period based on the length of dataArray
+    const scaleFactor = 10; // Adjust the scale factor for the height
+    const yOffset = 40; // Adjust the offset for vertical position
+    return Math.sin(index * period) * scaleFactor + yOffset;
+  };
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(prevCount => (prevCount % totalItems) + 1); // Increment count cyclically
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [totalItems]);
+
+  useEffect(() => {
+    setDataArray(ye[count.toString()]);
+  }, [count]);
+
+  useEffect(() => {
+    Animated.parallel(
+      animatedValues.map((value, index) =>
+        Animated.timing(value, {
+          toValue: calculateHeight(dataArray[index], index), // Use the calculateHeight function
+          duration: 1000,
+          useNativeDriver: false,
+        })
+      )
+    ).start();
+  }, [count, dataArray, animatedValues]);
   return (
-   <View>
+  <ImageBackground
+  source={{uri:"https://www.rollingstone.com/wp-content/uploads/2023/07/Burna-boy-new-album-big-7-song.jpg"}}
+  style={{
+    flex: 1,
+    resizeMode: "contain",
+    justifyContent: "space-between",
+  
+ 
+  }}
+  >
+   
+    <BlurView
+    style={{
+      height: "100%",
+      width: "100%",
+      backgroundColor:'transparent',
+   
+   
+    }}
+    intensity={100}
+    tint="dark"
+    >
+         <LinearGradient
+        colors={['#0000002e','#0000002e', '#000000d9', ]}
+   
+        start={[0, 0]}
+        end={[1, 1]}
+      
+        
+     
+      style={{
+        height: "100%",
+        width: "100%",
+        paddingVertical: 60,
+
     
-   </View>
+      }}
+      >
+        <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+                  paddingHorizontal: 20,
+          
+        }}
+        >
+          <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: 40,
+            width: 40,
+            borderRadius: 40,
+            backgroundColor: '#0000002e',
+          }}
+          >
+          <ArrowLeft2 size="22" color="#fff"/>
+          </View>
+          <Text
+          style={{
+            color: 'white',
+            fontSize: 15,
+            fontWeight: '500',
+            marginLeft: 4
+          }}
+          >
+            Now Playing
+          </Text>
+          <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: 40,
+            width: 40,
+            borderRadius: 40,
+            backgroundColor: '#0000002e',
+          }}
+          >
+          <Heart size="22" color="#fff"/>
+          </View>
+        </View>
+        <View
+        style={{
+          flexDirection:'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingVertical: 30
+        }}
+        >
+          <Image
+          source={{uri:"https://www.rollingstone.com/wp-content/uploads/2023/07/Burna-boy-new-album-big-7-song.jpg"}}
+          style={{
+            width: 380,
+            height: 380,
+            borderRadius: 10
+          }}
+          />
+          <View
+          style={{
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            maxWidth:'auto',
+            marginTop:30,
+          
+          }}
+          >
+            <Text
+            style={{
+              color: 'white',
+              fontSize: 36,
+              fontWeight: '600',
+              marginTop: 10
+            }}
+            >
+              Ye
+            </Text>
+            <Text
+            style={{
+              color: '#ddd',
+              fontSize: 20,
+              fontWeight: '300',
+              marginTop: 5
+            }}
+            >
+              Burna Boy
+            </Text>
+
+          </View>
+        </View>
+        <View style={{height:60, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingVertical: 10, width: '100%' }}>
+      {dataArray.map((item, index) => (
+        <Animated.View
+          key={index}
+          style={{
+            height: animatedValues[index],
+            width: 3,
+            backgroundColor: 'white',
+            borderRadius: 20,
+            marginRight: 10,
+            opacity: 0.5,
+          }}
+        />
+      ))}
+    </View>
+      </LinearGradient>
+
+    </BlurView> 
+   
+    
+  </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 60,
+    paddingHorizontal: 30,
+  
   },
 });
---
+++ mobile/landing.js
@@ -0,0 +1,229 @@
+export default function Landing(){
+    return (
+        <LinearGradient
+        colors={['#2b174b', '#192f43', '#010303']}
+        style={styles.container}
+        start={[0, 0]}
+        end={[1, 1]}
+      
+        
+      >
+        <View>
+        <View
+        style={{
+          flexDirection:'column'
+        }}
+        >
+          <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            paddingBottom: 30
+          }}
+          >
+            <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              backgroundColor: 'transparent',
+              paddingVertical: 2,
+              paddingHorizontal: 12,
+              borderRadius: 50,
+              borderWidth: 0.5,
+              borderColor: 'white',
+    
+            
+            }}
+            >
+            <HambergerMenu size="15" color="#fff"/>
+            <Text
+            style={{
+              color: 'white',
+              fontSize: 14,
+              fontWeight: '300',
+              marginLeft: 4
+            
+            }}
+            >
+              Menu
+            </Text>
+    
+            </View>
+            <View>
+              <Image
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 50,
+              
+                marginTop: 0
+              
+              }}
+              source={{uri:"https://s3.amazonaws.com/www-inside-design/uploads/2018/01/rick-morty-sq.jpg"}}
+              />
+            </View>
+          </View>
+          <Text
+          style={{ color: 'white', fontSize: 18, fontWeight: '300' }}
+          >
+            Hi, Welcome Back - Teddy 🔥
+          </Text>
+    
+          <View
+          style={{
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            marginTop: 10
+          
+          }}
+          >
+            <Text
+            style={{
+              color: 'white',
+              fontSize: 50,
+              fontWeight: '300'
+            }}
+            >
+              Listen To Your  
+            </Text>
+            <Text
+             style={{
+              color: 'white',
+              fontSize: 50,
+              fontWeight: '300',
+              fontStyle: 'italic',
+              textAlign: 'right'
+            }}
+            >
+              Favourite Music
+            </Text>
+          </View>
+          <View
+          style={{
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            marginTop: 20
+          }}
+          >
+       <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginTop: 20
+          }}
+          >
+            <Text
+            style={{
+              color: 'white',
+              fontSize: 16,
+              fontWeight: '600'
+            
+            }}
+            >
+              Rencently Played
+            </Text>
+            <Text
+            style={{
+              color: '#14b0c2',
+              fontSize: 14,
+              fontWeight: '400'
+            }}
+            >
+              See All
+            </Text>
+    
+          </View>
+    <ScrollView
+    showsHorizontalScrollIndicator={false}
+    horizontal={true}
+    style={{
+      marginTop: 10,
+     
+    }}
+    
+    >
+      {
+        [...Array(5)].map((item, index) => {
+          return (
+            <BlurView
+            style={{
+           
+              backgroundColor: '#7674742e',
+              borderRadius: 27,
+              paddingVertical: 20,
+              paddingHorizontal: 20,
+              width: Dimensions.get('window').width - 160,
+              marginRight: 20,
+              marginTop: 10,
+              flexDirection: 'row',
+              justifyContent: 'flex-start',
+              maxWidth:'auto',
+              alignItems:'flex-start',
+              
+          
+            }}
+            >
+              <View>
+            <Image
+            source={{uri:"https://www.rollingstone.com/wp-content/uploads/2023/07/Burna-boy-new-album-big-7-song.jpg"}}
+            style={{
+              width: 100,
+              height: 100,
+              borderRadius: 100
+            }}
+            />
+              </View>
+              <View
+              style={{
+                flexDirection: 'column',
+             
+                marginLeft: 10
+          
+              }}
+              >
+                <Text
+                style={{
+                  color: 'white',
+                  fontSize: 26,
+                  fontWeight: '600',
+                  marginTop: 10
+                }}
+                >
+                  Ye.
+                </Text>
+                <Text
+                style={{
+                  color: '#ddd',
+                  fontSize: 16,
+                  fontWeight: '400',
+                  marginTop: 5
+                }}
+                >
+                  Burna Boy
+                </Text>
+                <Text
+                style={{
+                  color: '#ddd',
+                  fontSize: 14,
+                  fontWeight: '400',
+                  marginTop: 5
+                }}
+          
+                >
+                03:45
+                </Text>
+          
+              </View>
+            </BlurView>
+          )
+      })}
+    
+    </ScrollView>
+          </View>
+        </View>
+        </View>
+      </LinearGradient>
+    )
+}
